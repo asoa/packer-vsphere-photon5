19ca9f6c13da4ce361aa0b5dfb467d100ac30024
asoa <abailey82@gmail.com>
2024-04-21 19:25:41 +0000

    ci

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -10,7 +10,6 @@ tdnf distro-sync -y &>/dev/null
 # install software
 tdnf install git docker-compose vim ansible bindutils tar -y &>/dev/null
 # download vscode cli 
-curl -sSL https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/script-library/common-alpine.sh | bash -s -- install
 curl -sSfL -o cli-alpine-x64.tgz "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64"
 tar -xzf cli-alpine-x64.tgz
 mv code /usr/local/bin/
